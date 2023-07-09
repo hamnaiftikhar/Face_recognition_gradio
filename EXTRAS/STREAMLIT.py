f7d54c6b23dd696182424cd962af8bd7cd74929a
Hamna Iftikhar <96309521+hamnaiftikhar@users.noreply.github.com>
2023-07-09 23:13:14 +0500

    Update STREAMLIT.py

--- EXTRAS/STREAMLIT.py
+++ EXTRAS/STREAMLIT.py
@@ -6,41 +6,14 @@ Created on Sun Jul  9 21:48:10 2023
 """
 
 import gradio as gr
-import random
-import nltk
-from nltk.corpus import gutenberg
+import time
+import streamlit as st
 
-# Get the text from the Gutenberg dataset
-dataset = gutenberg.raw()
 
-# Build Markov chain model
-def build_markov_model(dataset):
-    model = {}
-    sentences = nltk.sent_tokenize(dataset)
-    for sentence in sentences:
-        words = nltk.word_tokenize(sentence)
-        for i in range(len(words)-1):
-            if words[i] not in model:
-                model[words[i]] = []
-            model[words[i]].append(words[i+1])
-    return model
+def replace(text):
+    return text.replace('World', 'Databricks')
 
-# Generate next words using Markov chain model
-def generate_next_words(prompt):
-    words = nltk.word_tokenize(prompt)
-    if words[-1] in markov_model:
-        next_word = random.choice(markov_model[words[-1]])
-    else:
-        next_word = "<End of Text>"
-    return ' '.join(words + [next_word])
-
-# Build Markov chain model
-markov_model = build_markov_model(dataset)
-
-iface = gr.Interface(
-    fn=generate_next_words,
-    inputs=gr.inputs.Textbox(placeholder="Enter text"),
-    outputs=gr.outputs.Textbox(label="Generated Words")
-)
-
-iface.launch()
+st.title("Text Replacement")
+    input_text = st.text_area("Enter text", value="Hello, World!")
+    output_text = replace(input_text)
+    st.text_area("Replaced text", value=output_text)
